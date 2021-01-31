cdb9f6a737fd107ac533dfa888d7473d47c9e87e
lowee1 <63391242+lowee1@users.noreply.github.com>
2021-01-31 12:19:50 +0800

    Update webcam_cv3.py

--- webcam_cv3.py
+++ webcam_cv3.py
@@ -1,26 +1,37 @@
 import cv2
-import sys
-import logging as log
-import datetime as dt
 from time import sleep
 import statistics
 import wmi
-import win32api
+import tkinter as tk
 
-cascPath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascPath)
-log.basicConfig(filename='webcam.log',level=log.INFO)
+cascade_path = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascade_path)
 
 video_capture = cv2.VideoCapture(0)
 anterior = 0
 i = 0
-faceAreaSamples = []
-debugN = 10
+face_area_samples = []
+sleep_time = 10
+calibration_times = 10
 c = wmi.WMI(namespace='wmi')
 b = c.WmiMonitorBrightness()[0]
-startingBrightness = b.CurrentBrightness
+starting_brightness = b.CurrentBrightness
 
-brightnessMethods = c.WmiMonitorBrightnessMethods()[0]
+brightness_methods = c.WmiMonitorBrightnessMethods()[0]
+
+
+class Reminder(tk.Toplevel):
+    def __init__(self):
+        tk.Toplevel.__init__(self)
+        self.overrideredirect(True)
+        tk.Label(self, text="Too close to the screen").grid(row=0, column=0)
+        tk.Button(self, command=self.destroy, text="OK").grid(row=1, column=0)
+        self.lift()  # Puts Window on top
+        self.grab_set()  # Prevents other Tkinter windows from being used
+
+
+def show_reminder():
+    Reminder()
 
 while True:
     if not video_capture.isOpened():
@@ -41,43 +52,38 @@ while True:
     )
 
     if len(faces) == 1:
-        i = i + 1
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            print(i," ",h*w)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        i += 1
+        for (x, y, width, height) in faces:
+            print(i, " ", height*width)
+            cv2.rectangle(frame, (x, y), (x+width, y+height), (0, 255, 0), 2)
 
     if anterior != len(faces):
         anterior = len(faces)
-        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
 
-
-    # Display the resulting frame
     cv2.imshow('Video', frame)
 
-    if i < 5*debugN:
-        for (x,y,h,w) in faces:
-            faceAreaSamples.append(h*w/1000) 
+    if i == calibration_times:
+        mean = statistics.mean(face_area_samples)
+        stdev = statistics.stdev(face_area_samples)
+        print("calibration complete ", "mean:", mean, " stdev:", stdev)
+    elif i < calibration_times:
+        for (x, y, height, width) in faces:
+            face_area_samples.append(height*width/1000)
+    elif i > calibration_times:
+        if height*width/1000 > mean + stdev*3:
+            print("too close")
+            brightness_methods.WmiSetBrightness(25, 0)
+            show_reminder()
+            brightness_methods.WmiSetBrightness(starting_brightness, 0)
     else:
-        if i > 5*debugN:
-            #check stuff
-            if h*w/1000 > mean + stdev*3:
-                #alert
-                print("too close")
-                brightnessMethods.WmiSetBrightness(25, 0)
-                win32api.MessageBox(0,"You are too close to screen","Warning")
-                brightnessMethods.WmiSetBrightness(startingBrightness, 0)
-        else:
-            mean = statistics.mean(faceAreaSamples)
-            stdev = statistics.stdev(faceAreaSamples)
-            print("calibration complete ","mean:",mean," stdev:",stdev)
+        raise ValueError
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
-    sleep(1/debugN)
+    sleep(sleep_time)
 
 # When everything is done, release the capture
 video_capture.release()
